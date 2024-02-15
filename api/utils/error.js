08217c8d67ee53b8af23c9e535ef46c906dcd33a
Arshad <mdarshadkp786@gmail.com>
2024-02-15 18:00:40 +0400

    created signup api

---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+export const errorHandler = (statusCode,message) => {
+    const error = Error();
+
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
-    return (
-        <div>SignUp</div>
-    )
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="">
+        <div className="">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span
+              className="px-2 py-1 bg-gradient-to-r
+                 from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white"
+            >
+              Arsh
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
